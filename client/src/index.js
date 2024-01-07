b502544ccf15f5f3df006a933b149c218bce9862
rnutall <rnutall1@gmail.com>
2024-01-07 00:26:30 -0600

    committing added files

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import store from './utils/store';
+// import {render} from 'react-dom';
+// import { Provider } from 'react-redux'
+// import { applyMiddleware, createStore, compose } from 'redux'
+// import thunkMiddleware from 'redux-thunk'
+// import rootReducer from './reducers'
+// import loggerMiddleware from '..middleware/logger'
+// import monitorReducerEnhancer from  './enhancer/monitorReducer'
+// import './index.css';
+// import App from './App';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+//import * as serviceWorker from './serviceWorker';
+//* LOG THE INITIAL STATE
+console.log('Initial state:',store.getState() )
+// const middlewareEnhancer = applyMiddleware(loggerMiddleware, thunkMiddleware)
+// const composedEnhancers = compose(middlewareEnhancer, monitorReducerEnhancer)
+// const store = configureStore()
+const unsubscribe = store.subscribe(() =>
+  console.log('State after dispatch: ', store.getState())
+)
+store.dispatch({ type: 'utils/GlobalState', payload: 'useProductReducer' })
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.register();
+store.dispatch({
+type: 'filters/colorFilterChanged',
+  payload: { color: 'red', changeType: 'added' }
+})
+unsubscribe()
+
+store.dispatch({ type: 'utils/GlobalState' , payload:'useProductReducer'})
+// const renderApp = () =>
+// render(
+//   <Provider store={store}>
+//     <App />
+//   </Provider>,
+//   document.getElementById('root')
+// )
+// if (process.env.NODE_ENV !== 'production' && module.hot) {
+//   module.hot.accept('./components/App', renderApp)
+// }
+
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// renderApp();
